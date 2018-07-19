016d2212643574064f39733413ad7c331965e4cb
r1bb3t <r1bb3t@gmail.com>
2018-07-19 00:44:30 -0700

    updated julie mao file manager -- still wip

--- src/components/app/Devices/FileBrowser/RightSideBar.js
+++ src/components/app/Devices/FileBrowser/RightSideBar.js
@@ -7,16 +7,75 @@ class RightSideBar extends Component {
 
   render() {
     return (
-      <Col xs="3" className="col-tester3">
+      <Col xs="3" className="right">
         <div className="main--header">
           Something Something Something
         </div>
-        <ul className="main--list">
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-        </ul>
+        <Container fluid>
+          <Row>
+            <Col>
+              <div className="right-item right-item--first green-bg">
+                s
+              </div>
+              <div className="right-item--separator"></div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="right-item right-item--bulk green-bg">
+                s
+              </div>
+              <div className="right-item--separator"></div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="right-item right-item--bulk right-item--bulk mustard-bg">
+                s
+              </div>
+              <div className="right-item--separator"></div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="right-item right-item--bulk green-bg">
+                s
+              </div>
+              <div className="right-item--separator"></div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="right-item right-item--bulk mustard-bg">
+                s
+              </div>
+              <div className="right-item--separator"></div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="right-item right-item--bulk blue-bg">
+                s
+              </div>
+              <div className="right-item--separator"></div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="right-item right-item--bulk green-bg">
+                s
+              </div>
+              <div className="right-item--separator"></div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="right-item right-item--last green-bg">
+                s
+              </div>
+            </Col>
+          </Row>
+        </Container>
 
       </Col>
     );
